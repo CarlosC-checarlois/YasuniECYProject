e355ddd526b28aa92dc1f7dae5d264342ec02177
CARLOS C - CheCarlois <102504371+CarlosC-checarlois@users.noreply.github.com>
2026-02-03 20:24:51 -0500

    Change ALLOWED_HOSTS to allow all hosts

--- Yasuni/settings.py
+++ Yasuni/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["yasuniecy.com","www.yasuniecy.com"]
+ALLOWED_HOSTS = ["*"]
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
